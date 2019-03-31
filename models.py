4ef4a959ca4ddaef444405f0f8f54829a6f02d69
Peepers <eyepeepers@hotmail.com>
2019-03-31 16:29:01 +0700

    add png

--- models.py
+++ models.py
@@ -162,7 +162,7 @@ class World:
 
         self.mrcorn = MrCorn(self, 50, 150)
         self.floor_list = []
-        self.fire = Fire(self, self.width//2, -700, 700, 800)
+        self.fire = Fire(self, self.width//2, -600, 700, 800)
         self.platforms = self.gen_map(map_lv1)
         self.coins = self.gen_coin(lv1_coins)
         self.coin_point = 0
